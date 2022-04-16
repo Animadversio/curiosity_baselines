2dc9824d99b990a6b5587a0aab7895e41a1989fe
jingxuanf0214 <66086600+jingxuanf0214@users.noreply.github.com>
2022-04-16 16:49:01 -0400

    Update count.py

--- rlpyt/models/curiosity/count.py
+++ rlpyt/models/curiosity/count.py
@@ -20,14 +20,36 @@ class CountBasedReward(nn.Module):
             device='cpu'
             ):
 		super(CountBasedReward, self).__init__()
-
+        self.image_shape = image_shape
+        self.alpha = alpha
+        self.gamma = gamma
         self.device = torch.device('cuda:0' if device == 'gpu' else 'cpu')
-
+        self.count = {}
+        
+       
     def compute_bonus(self, next_observation, done):
-    	raise NotImplementedError
-    	return alpha * bonus
+    	#raise NotImplementedError
+        if not next_observation.shape[2:] == torch.Size([4,5,5]):
+            next_observation = next_observation[:,:,-1,:,:]
+            next_observation = next_observation.unsqueeze(2)
+        key = torch.flatten(next_observation)
+        if key not in self.count:
+            obs_count = 1
+        else:
+            obs_count = self.count[key]+1
+        rewards = torch.from_numpy(self.alpha / np.sqrt(ob_counts))
+        rewards *= done
+        return rewards
 
     def compute_loss(self, observations, valid):
-    	raise NotImplementedError
-        phi, predicted_phi, T, B = self.forward(observations, done=None)
-        return forward_loss
+    	#raise NotImplementedError
+        #phi, predicted_phi, T, B = self.forward(observations, done=None)
+        if not observations.shape[2:] == torch.Size([4,5,5]):
+            observations = next_observation[:,:,-1,:,:]
+            observations = next_observation.unsqueeze(2)
+        key = torch.flatten(observations)
+        if key not in self.count:
+            self.count[key] = 1
+        else:
+            self.count[key] += 1
+        return 0
